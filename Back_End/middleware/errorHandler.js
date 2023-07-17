170af6f9b35965c6f2027cb6fac3ec3947d886dc
Mekuuxxx <mo7malo107@gmail.com>
2023-07-17 19:57:50 +0300

    Fixing error handling issue

--- Back_End/middleware/errorHandler.js
+++ Back_End/middleware/errorHandler.js
@@ -1,5 +1,6 @@
+const { StatusCodes } = require('http-status-codes');
 const errorHandler = (err, req, res, next) => {
-    return res.status().json({ msg: err.message });
+    return res.status(StatusCodes.NOT_FOUND).json({ msg: err.message });
 };
 
 module.exports = errorHandler;
