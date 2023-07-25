415a94e4ce67838ff97624f953740a5f1ba015d6
MekuXXX <122784049+MekuXXX@users.noreply.github.com>
2023-07-25 18:21:13 +0300

    Update to publish on render

--- Front_End/src/scenes/breakdown/Breakdown.tsx
+++ Front_End/src/scenes/breakdown/Breakdown.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header/Header";
-import BreakdownChart from "../../components/BreakdownChart/BreakdownChart";
+import BreakdownChart from "../../components/breakdownChart/BreakdownChart";
 
 export default function Breakdown() {
     return (
